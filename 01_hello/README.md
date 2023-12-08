d216bb69f3345da01a7d358b8b5450ea841ee5d5
landryx <landry@lnts.me>
2023-12-08 20:53:24 +0000

    tiny_python_projects - ending champter 01

--- 01_hello/README.md
+++ 01_hello/README.md
@@ -1,4 +1,4 @@
 # Chapter 1: Hello, World!
-the objective is write a program that greets the world.
-The objective being to make sure that the program pass all the tests planned in the book.
+The objective is write a program that greets the world.
+The main goal being to make sure that the program pass all the tests planned in the book.
 
--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
-# Purpose: Say hello
+"""
+Author : Landry <contact@lnts.me>
+Date   : 2023-12-08
+Purpose: Rock the Casbah
+"""
 
 import argparse
 
+
+# ------------------------------------------------------------
 def get_args():
+    """Get the command line argument"""
 
     parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name', 
+    parser.add_argument('-n', '--name', metavar='name',
                         default='World', help='Name to greet')
     return parser.parse_args()
 
+
+# ------------------------------------------------------------
 def main():
-    
+    """Make a jazz noise here bro"""
+
     args = get_args()
     print('Hello, ' + args.name + '!')
 
+
+# ------------------------------------------------------------
 if __name__ == '__main__':
     main()
