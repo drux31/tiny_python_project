630933d49f0a53698c57f7b48014c4a40d3d75f6
landryx <landry@lnts.me>
2024-01-13 22:06:05 +0000

    tinypythonproject - ch09 adding the main code

--- 09_dial_a_curse/abuse.py
+++ 09_dial_a_curse/abuse.py
@@ -6,7 +6,7 @@ Purpose: Dial a curse
 """
 
 import argparse
-
+import random
 
 # --------------------------------------------------
 def get_args():
@@ -56,15 +56,26 @@ def main():
     """Heap abuse code"""
 
     args = get_args()
+    random.seed(args.seed)
        
-    adjectives = args.adjectives
-    insults = args.number
-    seed = args.seed
-
-    if not seed and adjectives == 2 and insults == 3:
-        print('You foul, false varlet!')
-        print('You filthy, instantiate fool!')
-        print('You lascivious, corrupt recreant!')
+    adjectives = """
+    bankrupt base caterwauling corrupt cullionly detestable dishonest false
+    filthsome filthy foolish foul gross heedless indistinguishable infected
+    insatiate irksome lascivious lecherous loathsome lubbery old peevish
+    rascaly rotten ruinous scurilous scurvy slanderous sodden-witted
+    thin-faced toad-spotted unmannered vile wall-eyed
+    """.strip().split()
+ 
+    nouns = """
+    Judas Satan ape ass barbermonger beggar block boy braggart butt
+    carbuncle coward coxcomb cur dandy degenerate fiend fishmonger fool
+    gull harpy jack jolthead knave liar lunatic maw milksop minion
+    ratcatcher recreant rogue scold slave swine traitor varlet villain worm
+    """.strip().split()
+ 
+    for _ in range(args.number):
+        adjs = ', '.join(random.sample(adjectives, k=args.adjectives))
+        print(f'You {adjs} {random.choice(nouns)}!')
     
 
 # --------------------------------------------------
